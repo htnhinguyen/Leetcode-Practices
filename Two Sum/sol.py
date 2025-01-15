09103f8bdd663ef89528d863a55e245b555ee53a
ddanhinie <126351211+ddanhinie@users.noreply.github.com>
2025-01-15 08:02:34 -0800

    Create sol.py

---
+++ Two Sum/sol.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap = {}
+        for i in range(len(nums)):
+            hmap[nums[i]] = i
+        for i in range(len(nums)):
+            if target - nums[i] in hmap and i != hmap[target-nums[i]]:
+                A = [i, hmap[target-nums[i]]]
+                return A
+         
